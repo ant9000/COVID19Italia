379f907e453010e0d60ab148adb94bcc9f38da85
Antonio Galea <antonio.galea@gmail.com>
2020-03-25 19:46:05 +0100

    calcolo dati per l'intera Italia

--- lib/models/covid19data.dart
+++ lib/models/covid19data.dart
@@ -68,45 +68,77 @@ class COVID19DataModel extends ChangeNotifier {
       _byRegion[record.denominazioneRegione].add(idx);
  //     print(json);
     }
+
     print("_records: ${_records.length}");
     print("_byDate: ${_byDate.keys.length}");
     print("_byRegion: ${_byRegion.keys.length}");
+
+    _byRegion["ITALIA"] = [];
+    for (var d in _byDate.keys) {
+      var record = Record(
+        data:                      d,
+        stato:                     "ITA",
+        codiceRegione:             0,
+        denominazioneRegione:      "ITALIA",
+        ricoveratiConSintomi:      0,
+        terapiaIntensiva:          0,
+        totaleOspedalizzati:       0,
+        isolamentoDomiciliare:     0,
+        totaleAttualmentePositivi: 0,
+        nuoviAttualmentePositivi:  0,
+        dimessiGuariti:            0,
+        deceduti:                  0,
+        totaleCasi:                0,
+        tamponi:                   0,
+      );
+      for (var idx in _byDate[d]) {
+        record = record + _records[idx];
+      }
+      var idx = _records.length;
+      _records.add(record);
+      _byDate[d].insert(0, idx);
+      _byRegion[record.denominazioneRegione].add(idx);
+    }
+
     notifyListeners();
   }
 
-  List<Record> lastDayRecords() {
-    if (_byDate.keys.isNotEmpty) {
-      var d = _byDate.keys.last;
-      print("LAST DAY: $d");
-      return List.unmodifiable(_byDate[d].map((idx) => _records[idx]));
+  List<DateTime> getDays() { return List.unmodifiable(_byDate.keys); }
+
+  DateTime lastDay() { return _byDate.keys.isNotEmpty ? _byDate.keys.last : null; }
+
+  List<Record> getDayRecords(DateTime day) {
+    if (_byDate.keys.contains(day)) {
+      return List.unmodifiable(_byDate[day].map((idx) => _records[idx]));
     }
     return [];
   }
 
+  List<Record> lastDayRecords() { return getDayRecords(lastDay()); }
+
   List<Record> getRegion(var region){
-    print("REGION: $region");
     return List.unmodifiable(_byRegion[region].map((idx) => _records[idx]));
   }
 }
 
 @immutable
 class Record {
-  final  DateTime data;
-  final  String   stato;
-  final  int      codiceRegione;
-  final  String   denominazioneRegione;
-  final  double   lat;
-  final  double   long;
-  final  int      ricoveratiConSintomi;
-  final  int      terapiaIntensiva;
-  final  int      totaleOspedalizzati;
-  final  int      isolamentoDomiciliare;
-  final  int      totaleAttualmentePositivi;
-  final  int      nuoviAttualmentePositivi;
-  final  int      dimessiGuariti;
-  final  int      deceduti;
-  final  int      totaleCasi;
-  final  int      tamponi;
+  final DateTime data;
+  final String   stato;
+  final int      codiceRegione;
+  final String   denominazioneRegione;
+  final double   lat;
+  final double   long;
+  final int      ricoveratiConSintomi;
+  final int      terapiaIntensiva;
+  final int      totaleOspedalizzati;
+  final int      isolamentoDomiciliare;
+  final int      totaleAttualmentePositivi;
+  final int      nuoviAttualmentePositivi;
+  final int      dimessiGuariti;
+  final int      deceduti;
+  final int      totaleCasi;
+  final int      tamponi;
 
   Record({
     this.data,
@@ -153,5 +185,26 @@ class Record {
           "positivi: ${this.totaleAttualmentePositivi} " +
           "guariti: ${this.dimessiGuariti} " +
           "deceduti: ${this.deceduti}";
+  }
+
+  Record operator +(Record other) {
+    return new Record(
+      data:                      this.data,
+      stato:                     this.stato,
+      codiceRegione:             this.codiceRegione,
+      denominazioneRegione:      this.denominazioneRegione,
+      lat:                       this.lat,
+      long:                      this.long,
+      ricoveratiConSintomi:      this.ricoveratiConSintomi + other.ricoveratiConSintomi,
+      terapiaIntensiva:          this.terapiaIntensiva + other.terapiaIntensiva,
+      totaleOspedalizzati:       this.totaleOspedalizzati + other.totaleOspedalizzati,
+      isolamentoDomiciliare:     this.isolamentoDomiciliare + other.isolamentoDomiciliare,
+      totaleAttualmentePositivi: this.totaleAttualmentePositivi + other.totaleAttualmentePositivi,
+      nuoviAttualmentePositivi:  this.nuoviAttualmentePositivi + other.nuoviAttualmentePositivi,
+      dimessiGuariti:            this.dimessiGuariti + other.dimessiGuariti,
+      deceduti:                  this.deceduti + other.deceduti,
+      totaleCasi:                this.totaleCasi + other.totaleCasi,
+      tamponi:                   this.tamponi + other.tamponi,
+    );
   }
 }
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
 import '../screens/home.dart';
 import '../screens/region.dart';
 import '../models/covid19data.dart';
@@ -8,7 +9,8 @@ Drawer buildDrawer(BuildContext context, String currentRoute) {
   var data = Provider.of<COVID19DataModel>(context);
   String region = ModalRoute.of(context).settings.arguments;
   var records = data.lastDayRecords();
-  var date = records.isNotEmpty ? records[0].data.toIso8601String().substring(0,10) : '';
+  final formatter = new DateFormat('dd/MM/yyyy');
+  var date = records.isNotEmpty ? formatter.format(records[0].data) : '';
   var tiles = <ListTile>[];
   for(var record in records){
     tiles.add(ListTile(
