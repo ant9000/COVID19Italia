38740cfa1edd379ce6be3d802fe0dbe69d70121d
Antonio Galea <antonio.galea@gmail.com>
2020-03-26 18:48:59 +0100

    icona, nome

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:italia/main.dart';
+import 'package:covid19italia/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
